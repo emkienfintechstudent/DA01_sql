fbc16bfd6f3f3b5f02f54a085abc3d5ced722121
emkienfintechstudent <147416326+emkienfintechstudent@users.noreply.github.com>
2023-10-10 21:26:24 +0700

    Update practice2.sql

--- practice2.sql
+++ practice2.sql
@@ -5,6 +5,21 @@ WHERE ID % 2 = 0
   SELECT COUNT(ID) - COUNT(DISTINCT CITY) FROM STATION
 --ex3
   Select ceil(AVG(Salary) - avg(replace(Salary,0,''))) from EMPLOYEES
+--ex4
+Select ROUND(SUM(item_count::decimal * order_occurrences) /SUM(order_occurrences),1)from items_per_order;
 --ex5
-SELECT ROUND(SUM(item_count::DECIMAL*order_occurrences)/SUM(order_occurrences),1) AS mean
-FROM items_per_order;
+select candidate_id FROM candidates 
+where skill in ('Python','Tableau','PostgreSQL')
+group by candidate_id
+having count(skill) =3
+ORDER BY candidate_id
+--ex6
+select user_id, MAX(Date(post_date)) - min(date(post_date))AS days_between from posts 
+where DATE_PART('year',post_date::date)=2021
+GROUP BY user_id
+having MAX(Date(post_date))- min(Date(post_date)) <>0
+order by user_id
+--ex7
+SELECT card_name, max(issued_amount) - min(issued_amount) as diffirent from monthly_cards_issued
+group by card_name 
+order by max(issued_amount) - min(issued_amount) desc
