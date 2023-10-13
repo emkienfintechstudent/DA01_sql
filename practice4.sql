e3127f0da83dec28a916ed0e7b6a0f370f502468
emkienfintechstudent <147416326+emkienfintechstudent@users.noreply.github.com>
2023-10-13 14:49:07 +0700

    Create practice4.sql

---
+++ practice4.sql
@@ -0,0 +1,24 @@
+---ex1
+SELECT sum(case when device_type = 'laptop' then 1
+ else 0 end) as laptop_views 
+,sum(case when device_type = 'tablet' or device_type ='phone' then 1
+ else 0 end) as mobile_views from viewership
+--ex2
+select x,y,z, case when x+y>z and x+z>y and y+z>x then 'Yes'
+else 'No' end as triangle
+from Triangle
+--ex3 
+select round(100.0* sum(CASE
+when call_category is null or call_category ='n/a' then 1
+else 0 end) /count(case_id),1) as   uncategorised_call_pct
+
+from callers
+--ex4 
+select name from Customer 
+where referee_id != 2 or referee_id is null
+--ex5
+select survived, sum(case when pclass =1 then 1 else 0 end ) as first_class,
+sum(case when pclass =2 then 1 else 0 end ) as second_class,
+sum(case when pclass =3 then 1 else 0 end ) as third_class
+from titanic
+group by survived
