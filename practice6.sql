1aa7b66d4c6350f2ddff0199293bd8c3cbb16698
emkienfintechstudent <147416326+emkienfintechstudent@users.noreply.github.com>
2023-10-18 00:29:15 +0700

    Update practice6.sql

--- practice6.sql
+++ practice6.sql
@@ -111,4 +111,101 @@ FROM Sales AS a
 JOIN min_year AS b ON a.product_id = b.product_id
 WHERE b.minyear = a.year
 
-/*--------------------------------------------------------EX7--------------------------------------------------------------------------*/ 
+/*--------------------------------------------------------EX8--------------------------------------------------------------------------*/ 
+WITH cte
+AS (
+	SELECT customer_id
+		,product_key
+	FROM Customer
+	GROUP BY customer_id
+		,product_key
+	)
+SELECT customer_id
+FROM cte
+GROUP BY customer_id
+HAVING count(*) = (
+		SELECT count(DISTINCT product_key)
+		FROM Product
+		)
+ORDER BY customer_id
+/*--------------------------------------------------------EX9--------------------------------------------------------------------------*/ 
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000
+	AND manager_id NOT IN (
+		SELECT employee_id
+		FROM Employees
+		)
+ORDER BY employee_id
+/*--------------------------------------------------------EX10 trùng EX 1--------------------------------------------------------------------------*/ 
+/*--------------------------------------------------------EX11 --------------------------------------------------------------------------*/
+-- Write your PostgreSQL query statement below
+WITH cte
+AS (
+	SELECT a.name AS results
+	FROM Users AS a
+	JOIN MovieRating AS b ON a.user_id = b.user_id
+	GROUP BY a.name
+	ORDER BY count(*) DESC
+		,a.name limit 1
+	)
+	,cte2
+AS (
+	SELECT c.title AS results
+	FROM Movies AS c
+	JOIN MovieRating AS d ON c.movie_id = d.movie_id
+	WHERE extract(month FROM d.created_at) = 2
+		AND extract(year FROM d.created_at) = 2020
+	GROUP BY c.title
+	ORDER BY avg(rating) DESC
+		,c.title limit 1
+	)
+SELECT *
+FROM cte
+
+UNION ALL
+
+SELECT *
+FROM cte2
+/*--------------------------------------------------------EX12 --------------------------------------------------------------------------*/
+WITH cte
+AS (
+	SELECT requester_id AS id
+		,count(*) AS count
+	FROM RequestAccepted
+	GROUP BY requester_id
+	)
+	,cte1
+AS (
+	SELECT accepter_id AS id
+		,count(*) AS count
+	FROM RequestAccepted
+	GROUP BY accepter_id
+	)
+SELECT id
+	,sum(count) AS num
+FROM (
+	SELECT *
+	FROM cte
+	
+	UNION ALL
+	
+	SELECT *
+	FROM cte1
+	)
+GROUP BY id
+ORDER BY num DESC limit 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
