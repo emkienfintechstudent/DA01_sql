d19a792fd56c47dad2c97744bd534d7d41d0d5da
emkienfintechstudent <147416326+emkienfintechstudent@users.noreply.github.com>
2023-10-19 23:58:13 +0700

    Update practice7.sql

--- practice7.sql
+++ practice7.sql
@@ -96,3 +96,45 @@ SELECT user_id
 		ELSE round(sum / 3::DECIMAL, 2)
 		END AS rolling_avg_3d
 FROM cte2
+-------------------------------------------------------------------EX6----------------------------------------------------------------------------------------------------
+	
+WITH cte
+AS (
+	SELECT merchant_id
+		,credit_card_id
+		,amount
+		,transaction_timestamp
+		,EXTRACT(hour FROM transaction_timestamp - lag(transaction_timestamp) OVER (
+				PARTITION BY merchant_id
+				,credit_card_id
+				,amount
+				)) * 60 + eXTRACT(minute FROM transaction_timestamp - lag(transaction_timestamp) OVER (
+				PARTITION BY merchant_id
+				,credit_card_id
+				,amount
+				)) AS TIME
+	FROM transactions
+	)
+SELECT COUNT(*) AS payment_count
+FROM cte
+WHERE TIME BETWEEN 0
+		AND 10
+-------------------------------------------------------------------EX7----------------------------------------------------------------------------------------------------
+with cte as (select category, product, sum(spend)  OVER(PARTITION BY category, product )
+from product_spend 
+where EXTRACT(year from transaction_date) =2022), -- lấy ra năm 2022 đã 
+cte2 as (
+SELECT category,product,sum as total_spend,RANK() OVER(PARTITION BY category ORDER BY sum desc) as rank from cte
+group by category,product,sum) -- lấy ra tổng và xếp hạng 
+select category,product, total_spend from cte2 
+where rank = 1 or rank =2 -- lấy ra 2 cái cao nhất 
+-------------------------------------------------------------------EX8----------------------------------------------------------------------------------------------------
+with cte as(SELECT artist_name,dense_RANK() OVER(ORDER BY count(*) desc) as artist_rank FROM artists as a 
+	join songs as b on a.artist_id=b.artist_id 
+	join global_song_rank as c
+on b.song_id = c.song_id
+where rank <= 10 
+group by artist_name)
+
+select artist_name,artist_rank from cte 
+where artist_rank <=5
